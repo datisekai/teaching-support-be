844eab31553f63af1014b0ef6a66877d20501729
Thành Đạt <datly030102@gmail.com>
2024-07-13 17:35:21 +0700

    fix

--- src/services/ColyseusService.ts
+++ src/services/ColyseusService.ts
@@ -1,59 +1,58 @@
-import { Room, matchMaker } from "colyseus";
-import { RoomService } from "./RoomService";
-export class ColyseusService {
-  static createRoom = async (data: any) => {
-    try {
-      const reservation = await matchMaker.create("event_room", {
-        data,
-        allowReconnectionTime: 40,
-      });
+// import { Room, matchMaker } from "colyseus";
+// export class ColyseusService {
+//   static createRoom = async (data: any) => {
+//     try {
+//       const reservation = await matchMaker.create("event_room", {
+//         data,
+//         allowReconnectionTime: 40,
+//       });
 
-      return reservation;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  };
-  static disconnectRoom = async (roomId: number) => {
-    try {
-      const rooms = await matchMaker.query({ name: "event_room" });
-      let room = null;
-      rooms.forEach((item) => {
-        if (item.metadata.id == roomId) {
-          room = item;
-        }
-      });
+//       return reservation;
+//     } catch (error) {
+//       console.log(error);
+//       return false;
+//     }
+//   };
+//   static disconnectRoom = async (roomId: number) => {
+//     try {
+//       const rooms = await matchMaker.query({ name: "event_room" });
+//       let room = null;
+//       rooms.forEach((item) => {
+//         if (item.metadata.id == roomId) {
+//           room = item;
+//         }
+//       });
 
-      if (room) {
-        await room.remove();
-        return true;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-    return false;
-  };
-  static initRoomAfterRestart = async () => {
-    const activeRooms = await RoomService.getAll();
-    for (const room of activeRooms) {
-      await this.createRoom(room);
-    }
-  };
-  static findRoomId = async (data: any) => {
-    console.log("findRoomId", data);
-    try {
-      const rooms = await matchMaker.query({ name: "event_room" });
-      let room = null;
-      rooms.forEach((item) => {
-        if (item.metadata.id == data.id) {
-          room = item;
-        }
-      });
-      if (room) return room.roomId;
-      return null;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  };
-}
+//       if (room) {
+//         await room.remove();
+//         return true;
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     return false;
+//   };
+//   static initRoomAfterRestart = async () => {
+//     const activeRooms = await RoomService.getAll();
+//     for (const room of activeRooms) {
+//       await this.createRoom(room);
+//     }
+//   };
+//   static findRoomId = async (data: any) => {
+//     console.log("findRoomId", data);
+//     try {
+//       const rooms = await matchMaker.query({ name: "event_room" });
+//       let room = null;
+//       rooms.forEach((item) => {
+//         if (item.metadata.id == data.id) {
+//           room = item;
+//         }
+//       });
+//       if (room) return room.roomId;
+//       return null;
+//     } catch (error) {
+//       console.log(error);
+//       return false;
+//     }
+//   };
+// }
--- src/services/ItemService.ts
+++ src/services/ItemService.ts
@@ -1,21 +1,21 @@
-import { FindOneOptions } from "typeorm";
-import { myDataSource } from "../app-data-source";
-import { Item } from "../entity/item.entity";
+// import { FindOneOptions } from "typeorm";
+// import { myDataSource } from "../app-data-source";
+// import { Item } from "../entity/item.entity";
 
-export class ItemService {
-  static getOne = async (query: FindOneOptions<Item>) => {
-    const itemRepository = myDataSource.getRepository(Item);
-    return itemRepository.findOne(query);
-  };
-  static updateQuantity = async (item_id: number) => {
-    const itemRepository = myDataSource.getRepository(Item);
-    const item = await this.getOne({
-      where: { id: item_id, is_deleted: false },
-    });
+// export class ItemService {
+//   static getOne = async (query: FindOneOptions<Item>) => {
+//     const itemRepository = myDataSource.getRepository(Item);
+//     return itemRepository.findOne(query);
+//   };
+//   static updateQuantity = async (item_id: number) => {
+//     const itemRepository = myDataSource.getRepository(Item);
+//     const item = await this.getOne({
+//       where: { id: item_id, is_deleted: false },
+//     });
 
-    if (item && item.quantity_remain > 0) {
-      item.quantity_remain -= 1;
-      await itemRepository.save(item);
-    }
-  };
-}
+//     if (item && item.quantity_remain > 0) {
+//       item.quantity_remain -= 1;
+//       await itemRepository.save(item);
+//     }
+//   };
+// }
