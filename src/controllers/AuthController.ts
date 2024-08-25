e20966da09464a2a494a5cd63dcb9f22ef5bf06b
Thành Đạt <datly030102@gmail.com>
2024-08-25 17:30:58 +0700

    fix: sync data ttdt when password null

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -27,7 +27,7 @@ class AuthController {
         select: ["id", "active", "code", "email", "password", "role", "name"],
       });
 
-      if (!user) {
+      if (!user || (user && !user.password)) {
         const info = await AuthService.getInformation(code, password);
 
         if (!info) {
